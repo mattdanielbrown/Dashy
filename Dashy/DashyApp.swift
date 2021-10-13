4b2a1186cb39aac28ddc03ff60eff783b20f026a
Aayush9029 <aayushpokharel36@gmail.com>
2021-10-13 13:33:56 -0400

    Initial Commit

---
+++ Dashy/DashyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashyApp.swift
+//  Dashy
+//
+//  Created by Aayush Pokharel on 2021-10-13.
+//
+
+import SwiftUI
+
+@main
+struct DashyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
